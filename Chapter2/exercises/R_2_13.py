d7dc4d8839d01d704424af23a12f4507d3c7e3b4
canglangxing <1240184827@qq.com>
2021-11-24 20:54:15 +0800

    add

---
+++ Chapter2/exercises/R_2_13.py
@@ -0,0 +1,50 @@
+class Vector:
+
+    def __init__(self, d):
+        self._coords = [0] * d
+
+    def __len__(self):
+        return len(self._coords)
+
+    def __getitem__(self, j):
+        return self._coords[j]
+
+    def __setitem__(self, j, val):
+        self._coords[j] = val
+
+    def __add__(self, other):
+        if len(self) != len(other):
+            raise ValueError('dimensions must agree')
+        result = Vector(len(self))
+        for j in range(len(self)):
+            result[j] = self[j] + other[j]
+        return result
+
+    def __mul__(self, other):
+        result = Vector(len(self))
+        for j in range(len(self)):
+            result[j] = self[j] * other
+        return result
+
+    def __rmul__(self, other):
+        return self * other
+
+    def __eq__(self, other):
+        return self._coords == other._coords
+
+    def __ne__(self, other):
+        return not self == other
+
+    def __str__(self):
+        return '<' + str(self._coords)[1:-1] + '>'
+
+
+if __name__ == '__main__':
+    v = Vector(5)
+    for i in range(5):
+        v[i] = i
+
+    u1 = v * 3
+    u2 = 3 * v
+    print(u1)
+    print(u2)
